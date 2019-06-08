55620ac4efc837f4a13ab7c377228b9e0e7e66a6
Unknown <lean1998@hotmail.com>
2019-06-08 10:53:06 -0300

    PlayerStructs

---
+++ Eterjam-2019/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public struct Stats
+    {
+        public int life;
+        public int speedMovement;
+    }
+
+    public struct Controls
+    {
+        public KeyCode upKey;
+        public KeyCode downKey;
+        public KeyCode leftKey;
+        public KeyCode rightKey;
+        public string horizontalAxis;
+        public string verticalAxis;
+    }
+
+    public Controls controls;
+    public Stats stats;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
