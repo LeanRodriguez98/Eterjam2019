4685e7413e5d13f5b8d2672eb7ed2dd4a158dc70
Unknown <lean1998@hotmail.com>
2019-06-08 17:16:50 -0300

    AddedBatEnemy

---
+++ Eterjam-2019/Assets/Scripts/Enemies/BatEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatEnemy : Enemy {
+
+    public float horizontalSpeed;
+    public float LeftRightTime;
+
+    public float UpDownTime;
+    public float verticalSpeed;
+
+    // Use this for initialization
+    override public void Start() {
+        base.Start();
+        Invoke("ChangeDirection", LeftRightTime);
+    }
+
+    // Update is called once per frame
+    override public void Update() {
+        base.Start();
+        Movement();
+    }
+
+
+    public void Movement()
+    {
+        transform.position += Vector3.right * horizontalSpeed * stats.speedMovement * Time.deltaTime;
+        transform.position += Vector3.up * verticalSpeed * stats.speedMovement * Mathf.Sin(Time.timeSinceLevelLoad);
+    }
+    public void ChangeDirection()
+    {
+        horizontalSpeed *= -1;
+        Invoke("ChangeDirection", LeftRightTime);
+    }
+}
---
+++ Eterjam-2019/Assets/Scripts/PresurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresurePlate : MonoBehaviour {
+
+    private bool canSpawn = true;
+    public GameObject enemyToSpawn;
+    public Transform[] positionsToSpawnEnemies;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (canSpawn)
+            {
+                SpawnEnemies();
+                canSpawn = false;
+            }
+        }
+    }
+
+    public void SpawnEnemies()
+    {
+        for (int i = 0; i < positionsToSpawnEnemies.Length; i++)
+        {
+            Instantiate(enemyToSpawn, positionsToSpawnEnemies[i].position, transform.rotation);
+        }
+    }
+}
