994f20f8b0d5263ee6a16ea1b07ee6a14eb356fa
Unknown <lean1998@hotmail.com>
2019-06-08 15:13:54 -0300

    CameraMovement

--- Eterjam-2019/Assets/Scripts/CameraFollow.cs
+++ Eterjam-2019/Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    public Transform target;
+    private float offsetY;
+
 	// Use this for initialization
 	void Start () {
-		
+        offsetY = target.position.y - transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (offsetY != target.position.y - transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - offsetY, transform.position.z);
+            offsetY = target.position.y - transform.position.y;
+
+        }
+    }
 }
