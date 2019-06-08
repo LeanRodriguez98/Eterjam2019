80b904bde5536e7abb77f2248704463d613385c8
Unknown <lean1998@hotmail.com>
2019-06-08 11:33:38 -0300

    CreditsScreen

---
+++ Eterjam-2019/Assets/Scripts/UI/UI_Credits.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Credits : MonoBehaviour {
+
+    public void LoadScene(string sceneName)
+    {
+        Utilities.LoadScene(sceneName);
+    }
+}
