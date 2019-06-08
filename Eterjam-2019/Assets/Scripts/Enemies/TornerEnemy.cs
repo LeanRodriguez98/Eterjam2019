762fc3efdf04f54d6ae7f82d94126aca4a428238
Unknown <lean1998@hotmail.com>
2019-06-08 15:34:15 -0300

    StuffInFloors

---
+++ Eterjam-2019/Assets/Scripts/Enemies/TornerEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornerEnemy : Enemy {
+
+    public GameObject bullet;
+
+    override public void Start () {
+        base.Start();
+	}
+	
+	override public void Update () {
+        base.Update();
+	}
+
+    public void Attack()
+    {
+        
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+
+}
