56360ef4614646f6e89e86e0a70c4e5d4c13cfec
Unknown <lean1998@hotmail.com>
2019-06-09 12:06:13 -0300

    TheBossCanDoDamage

---
+++ Eterjam-2019/Assets/Scripts/Enemies/BossArm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossArm : MonoBehaviour {
+
+    private Player playerReference;
+    private Boss boss;
+    private void Start()
+    {
+        playerReference = Player.playerInstance;
+        boss = GetComponentInParent<Boss>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playerReference != null)
+        {
+            if (collision.gameObject == playerReference.gameObject)
+            {
+                playerReference.SetDamage(boss.damage);
+            }
+        }
+    }
+}
