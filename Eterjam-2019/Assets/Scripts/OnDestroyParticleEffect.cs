9a29a1e6ac86e43e18e9f4ebf6e4cc3cfb18c201
Unknown <jquintanamartin@hotmail.com>
2019-06-09 12:03:42 -0300

    DoorDestroy Particles

---
+++ Eterjam-2019/Assets/Scripts/OnDestroyParticleEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDestroyParticleEffect : MonoBehaviour {
+
+    public GameObject particleGameObject;
+
+    private void Awake()
+    {
+        if (particleGameObject.GetComponent<ParticleSystem>()) {
+            Debug.LogWarning("El GameObject Asignado no contiene un Particle System");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(particleGameObject, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
