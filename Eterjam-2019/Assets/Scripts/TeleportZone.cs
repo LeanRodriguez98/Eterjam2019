9e260b4cac95cb3de2943b6d4e4fd03aedf22d2c
Unknown <lean1998@hotmail.com>
2019-06-09 15:17:01 -0300

    BossChangeMusic

--- Eterjam-2019/Assets/Scripts/TeleportZone.cs
+++ Eterjam-2019/Assets/Scripts/TeleportZone.cs
@@ -7,13 +7,20 @@ public class TeleportZone : MonoBehaviour {
 
     public GameObject objectToTeleport;
     public Transform teleportPosition;
-
+    public AudioClip audioClip;
+    public AudioSource AudioSource;
+    public bool changeMusic = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == objectToTeleport)
         {
             objectToTeleport.transform.position = teleportPosition.position;
+            if (changeMusic)
+            {
+                AudioSource.clip = audioClip;
+                AudioSource.Play();
+            }
         }
     }
 }
